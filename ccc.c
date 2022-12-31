1753177b0e2a38c6b90557248cc52f63cd2c7cea
cospplredman <cospplredman@gmail.com>
2022-12-31 00:38:34 -0600

    revamped testing executable

--- ccc.c
+++ ccc.c
@@ -3,25 +3,6 @@
 #include<stdlib.h>
 #include"src/ast.h"
 
-const char *tokenName[] = {
-	"T_AUTO", "T_ENUM", "T_RESTRICT", "T_UNSIGNED", "T_BREAK", "T_EXTERN", "T_RETURN", "T_VOID", "T_CASE", "T_FLOAT", "T_SHORT", "T_VOLATILE", "T_CHAR", "T_FOR", "T_SIGNED", "T_WHILE", "T_CONST", "T_GOTO", "T_SIZEOF", "T_BOOL", "T_CONTINUE", "T_IF", "T_STATIC", "T_COMPLEX", "T_DEFAULT", "T_INLINE", "T_STRUCT", "T_IMAGINARY", "T_DO", "T_INT", "T_SWITCH", "T_DOUBLE", "T_LONG", "T_TYPEDEF", "T_ELSE", "T_REGISTER", "T_UNION",
-	"T_IDENTIFIER",
-	
-	"T_INTEGERCONST",
-	"T_FLOATCONST",
-	"T_CHARCONST",
-
-	"T_STRINGLIT",
-
-	"T_LBRACE", "T_RBRACE", "T_LPAREN", "T_RPAREN", "T_LCURLY", "T_RCURLY", "T_DOT", "T_ARROW", "T_INC", "T_DEC", "T_AND", "T_STAR", "T_PLUS", "T_MINUS", "T_TILDE", "T_BANG", "T_SLASH", "T_PERCENT", "T_LSHIFT", "T_RSHIFT", "T_LT", "T_GT", "T_LTEQ", "T_GTEQ", "T_EQUAL", "T_NOTEQUAL", "T_XOR", "T_OR", "T_BAND", "T_BOR", "T_QMARK", "T_COLON", "T_SEMI", "T_ELIPSIS", "T_EQUALS", "T_EQ", "T_MULEQ", "T_DIVEQ", "T_MODEQ", "T_ADDEQ", "T_SUBEQ", "T_LSHIFTEQ", "T_RSHIFTEQ", "T_ANDEQ", "T_XOREQ", "T_OREQ", "T_COMMA", "T_POUND", "T_PPCONCAT",
-
-	"T_PPHEADER",
-
-	"T_WS",
-	"T_PPEXTRA"
-};
-
-
 char* readfile(FILE *f) {
   // f invalid? fseek() fail?
   if (f == NULL || fseek(f, 0, SEEK_END)) {
@@ -54,25 +35,20 @@ int main(int argc, char **argv){
 
 	for(int i = 0; i < argc; i++){
 		FILE * cf = fopen(argv[i], "r");
-		
 		char *str = readfile(cf), *strp = str;
 
-
 		Token *tok = genTokens(&strp);
-
-		Token *tmp = tok;
-		while(tmp->token != T_PPEXTRA){
-			printf("%-15s: %d %lf \"%.*s\"\n", tokenName[tmp->token], tmp->intValue, tmp->floatValue, tmp->end - tmp->start, tmp->start);
-			tmp = tmp->next;
-		}
+		printTokens(tok);
 
 		AST *ast = genAST(&tok);
 		if(!ast)
 			printf("could not generate ast\n");
+		else
+			printAST(ast);
 		
 		
-		//TODO free tok
-		//TODO free ast
+		freeToken(tok);
+		freeAST(ast);
 		free(str);
 		fclose(cf);
 	}
